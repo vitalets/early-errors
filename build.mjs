85c5d797b0bbcc1287266daada83225bbb76cdef
Vitaliy Potapov <noginsk@rambler.ru>
2023-11-30 15:26:53 +0400

    output to dist

--- build.mjs
+++ build.mjs
@@ -5,7 +5,7 @@ const pkg = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
 
 await esbuild.build({
   entryPoints: ['src/index.js'],
-  outfile: 'src/index.min.js',
+  outfile: 'dist/index.min.js',
   target: ['chrome58', 'edge16', 'firefox57', 'safari11'],
   banner: {
     js: `/* early-errors v${pkg.version} */`,
